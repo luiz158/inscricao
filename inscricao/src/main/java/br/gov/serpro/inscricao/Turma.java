d961e7de407142aee9e3926e48d1b2023786afa4
Luiz Augusto <luiz158@gmail.com>
2011-11-11 14:06:16 +0000

    modulo4 - video 3 - teste com hsqldb ok

--- inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
+++ inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 import org.slf4j.Logger;
 
@@ -18,7 +19,7 @@ import br.gov.serpro.inscricao.exception.TurmaException;
 public class Turma {
 
 	//private List<String> alunosMatriculados = new ArrayList<String>();
-	private List<Aluno> alunosMatriculados = new ArrayList<Aluno>();
+	//private List<Aluno> alunosMatriculados = new ArrayList<Aluno>();
 	
 	@Inject
 	private Logger logger; // = LoggerFactory.getLogger(Turma.class);
@@ -29,6 +30,9 @@ public class Turma {
 	@Inject
 	private InscricaoConfig config;
 	
+	@Inject
+	private EntityManager em;   // obs.: pode ser injectado em EM devido à extensão demoiselle-jpa
+	
 	public void matricular(Aluno aluno) {
 		if( estaMatriculado(aluno) ) {
 			logger.info(bundle.getString("aluno.ja.matriculado", aluno.getNome()));
@@ -37,20 +41,26 @@ public class Turma {
 		}
 		
 		//if( alunosMatriculados.size() >= 5 ) {
-		if( alunosMatriculados.size() >= config.getCapacidadeTurma() ) {
+		//if( alunosMatriculados.size() >= config.getCapacidadeTurma() ) {
+		if( obterAlunosMatriculados().size() >= config.getCapacidadeTurma() ) {
 			logger.info(bundle.getString("turma.ja.completa", aluno.getNome()));
 			//throw new RuntimeException();
 			throw new TurmaException();
 		}
 		
-		alunosMatriculados.add(aluno);
+		//alunosMatriculados.add(aluno);
+		em.getTransaction().begin();
+		em.persist(aluno);
+		em.getTransaction().commit();
+		
 		//System.out.println("Aluno matriculado com sucesso!");
 		//logger.info("Aluno matriculado com sucesso!");
 		logger.info(bundle.getString("matricula.sucesso", aluno.getNome()));
 	}
-
+	
 	public boolean estaMatriculado(Aluno aluno) {
-		return alunosMatriculados.contains(aluno);
+		//return alunosMatriculados.contains(aluno);
+		return obterAlunosMatriculados().contains(aluno);
 	}
 	
 	@ExceptionHandler     // deve anotar a classe com @Controller
@@ -58,6 +68,10 @@ public class Turma {
 	public void tratar(TurmaException e) {
 		logger.warn(bundle.getString("matricula.erro"));
 		throw e;
+	}
+	
+	public List<Aluno> obterAlunosMatriculados() {
+		return em.createQuery("select a from Aluno a").getResultList();
 	}
 
 }
