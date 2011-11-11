1ea42088b96b40d85d5014e83d45db99b71302f1
Luiz Augusto <luiz158@gmail.com>
2011-11-11 11:56:44 +0000

    modulo1-introducao - video 2 - parte 1

---
+++ inscricao/src/main/java/br/gov/serpro/inscricao/Turma.java
@@ -0,0 +1,15 @@
+package br.gov.serpro.inscricao;
+
+public class Turma {
+
+	public void matricular(String aluno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean estaMatriculado(String aluno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- inscricao/src/test/java/br/gov/serpro/inscricao/TurmaTest.java
+++ inscricao/src/test/java/br/gov/serpro/inscricao/TurmaTest.java
@@ -1,10 +1,16 @@
 package br.gov.serpro.inscricao;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 public class TurmaTest {
 	@Test
 	public void matricularAlunoComSucesso() {
+		Turma turma = new Turma();
+		
+		turma.matricular("Santos Dumont");
+		Assert.assertTrue(turma.estaMatriculado("Santos Dumont"));
 	}
 
 	@Test
