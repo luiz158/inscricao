40fccbed9c05ceaf5d76712f41800468d356fb6a
Luiz Augusto <luiz158@gmail.com>
2011-11-11 14:18:07 +0000

    modulo4 - video 5 - teste 1 - @ApplicationExcepiton(rollback=fase) - sem erro - pero.....

--- inscricao/src/main/java/br/gov/serpro/inscricao/exception/TurmaException.java
+++ inscricao/src/main/java/br/gov/serpro/inscricao/exception/TurmaException.java
@@ -1,5 +1,8 @@
 package br.gov.serpro.inscricao.exception;
 
+import br.gov.frameworkdemoiselle.exception.ApplicationException;
+
+@ApplicationException(rollback=false)
 public class TurmaException extends RuntimeException {
 
 }
